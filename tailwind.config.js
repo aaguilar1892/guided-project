b6ec56b1daeef0a6391b7ea1cf40b576fcf8a904
Alex Aguilar <aaguilar1892@gmail.com>
2024-11-30 02:39:13 -0600

    setting up fonts and background colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        pastelblue: '#A1CAF1',
+        pastelgreen: '#B0E57C',
+        pastelpink: '#FDC1C5',
+        darkgreen: '#355E3B', 
+        darkblue: '#003366',
+      },
+    },
   },
   plugins: [],
 }
