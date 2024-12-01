9c2e0ca819730bc49276f050accb32af1f28506f
Alex Aguilar <aaguilar1892@gmail.com>
2024-11-30 19:54:38 -0600

    making cards in react

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import FeaturedItem from './components/FeaturedItem'
+import Card from './components/Card'
 
 function App() {
   return (
     <div>
       <Navbar />
       <FeaturedItem />
+      <Card />
     </div>
   )
 }
