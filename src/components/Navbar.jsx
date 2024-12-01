b4f0d09cde568af5ec0d45a663c5ffc34af34cdd
Alex Aguilar <aaguilar1892@gmail.com>
2024-11-30 18:41:50 -0600

    finished 'your first component' section

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+// Navbar.jsx
+import React from "react";
+
+function Navbar() {
+  return (
+    <>
+        <div className="flex flex-row justify-between items-center px-20 py-4 bg-pastelblue">
+            <div className="text-darkgreen text-3xl poppins-bold hover:text-pastelgreen transition duration-300">
+                Ripple & Refresh
+            </div>
+            <div className="flex flex-row gap-8 poppins-semibold text-2xl text-darkblue">
+                <p className="hover:text-pastelgreen transition duration-300">map</p>
+                <p className="hover:text-pastelgreen transition duration-300">rsvp</p>
+                <p className="hover:text-pastelgreen transition duration-300">activity</p>
+            </div>
+        </div>
+        <hr className="w-full border-t-2 border-pastelgreen border-opacity-50" />
+    </>
+  );
+}
+
+export default Navbar;
